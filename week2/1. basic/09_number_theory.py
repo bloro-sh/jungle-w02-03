0915bd79037dcb5a0e5c504c3f3a815f35b7350e
bloro <317373918+bloro-sh@users.noreply.github.com>
2026-09-02 00:38:19 +0900

    basic 09_number_theory 문제풀이

--- week2/1. basic/09_number_theory.py
+++ week2/1. basic/09_number_theory.py
@@ -24,6 +24,7 @@
 - LCM 공식: lcm(a, b) = (a × b) / gcd(a, b)
 """
 
+
 def gcd(a, b):
     """
     유클리드 호제법을 사용한 최대공약수 계산
@@ -34,12 +35,20 @@ def gcd(a, b):
     Returns:
         최대공약수
     """
+    if b == 0:
+        return a
+    
+    return gcd(b,a%b)
+
     # TODO: 유클리드 호제법 구현
     # base case: b가 0이면 a 반환
-    # recursive를 이용 
+    # recursive를 이용
     pass
 
+
 def gcd_iterative(a, b):
+    # (공약수:공통인 약수) 공통인 약수중에서 가장 큰 약수
+
     """
     반복문을 사용한 최대공약수 계산
     
@@ -49,9 +58,21 @@ def gcd_iterative(a, b):
     Returns:
         최대공약수
     """
+
+    #작은수범위 구하기
+    small = min(a,b)#48,18 = 18
+    for i in range(1,small,+1): #시작이 1,끝:최소값 a,b,1더한 간격 1~18
+        if a % i == 0 and b % i == 0 : 
+            result = i
+
+    return result
+
+
     # TODO: 반복문으로 구현
     # b가 0이 될 때까지 반복
     pass
+
+
 
 def lcm(a, b):
     """
@@ -63,8 +84,16 @@ def lcm(a, b):
     Returns:
         최소공배수
     """
+    # 최소공배수는 배수중에서 제일 작은수 인데 배수는 값이 계속 확장하니까 처음공통된 수를 구해야된다
+    # max로 큰수를 먼저 구해서 시작하고 큰 수부터 두 수의 곱까지 확인
+    for i in range(max(a,b),(a*b)+1):
+        if i % a == 0 and i % b == 0:#나누어 떨어지는게 같은지 비교
+            return i
+    
     # TODO: LCM 계산
     pass
+
+
 
 def extended_gcd(a, b):
     """
@@ -77,11 +106,20 @@ def extended_gcd(a, b):
     Returns:
         (gcd, x, y) 튜플
     """
+    if b == 0:
+        return a,1,0 #(a,x(1),y(0))
+    else:
+        g,x,y = extended_gcd(b,a%b)
+        return g,y,x-(a//b)*y #최대공약수,x,y값 구하기
+    #6,1,
+
     # TODO: 확장 유클리드 호제법 구현
     # base case: b가 0이면 (a, 1, 0) 반환    
     # recursive case
     # 역추적하며 x, y 계산
     pass
+
+
 
 def is_prime(n):
     """
@@ -93,11 +131,23 @@ def is_prime(n):
     Returns:
         소수이면 True, 아니면 False
     """
+    if n < 2:
+        return False
+
+    for i in range(2,int(n**0.5)+1): #제곱근 이하에서 무조건 약수 하나가 발견
+        if n % i == 0: #나누어떨어지면 소수가 아니다
+            return False
+
+    return True
+
+    #소수 유일한 값 -> 1과 자기자신을 제외하고 나머지가 나눠지는 수가 있는지?
     # TODO: 소수 판별 구현
     # n이 2보다 작으면 False
-    # 2부터 sqrt(n)까지 나누어 떨어지는지 확인    
+    # 2부터 sqrt(n)까지 나누어 떨어지는지 확인
     # 3부터 sqrt(n)까지 홀수만 확인
-    pass 
+
+    pass
+
 
 # 테스트 케이스
 if __name__ == "__main__":
