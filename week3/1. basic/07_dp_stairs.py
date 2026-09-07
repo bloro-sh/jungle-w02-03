6d82f4cc25a024d131abe58d12e3d61160431a5e
bloro <317373918+bloro-sh@users.noreply.github.com>
2026-09-07 16:12:02 +0900

    week3 basic 07 상향식문제풀이

--- week3/1. basic/07_dp_stairs.py
+++ week3/1. basic/07_dp_stairs.py
@@ -45,13 +45,22 @@ def climb_stairs(n):
     Returns:
         n번째 계단까지 오르는 방법의 수
     """
+
+    dp = [0] * (n+1)
+
     # TODO: 특별한 경우 처리
     pass
-    
-    
+
+    if n == 0:
+        return 1
+    dp[0] = 1 #아무 계단 오르지않을때 (초기값 설정)
+    dp[1] = 1
+
     # TODO: dp 배열 생성 및 초기화
     pass
-    
+
+    for i in range(2,n+1):
+        dp[i] = dp[i-1]+dp[i-2]
     # TODO: 작은 문제부터 차례로 계산
     pass
     
